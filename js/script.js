da1a4c4daf1a10cff731ea24abe8d54af826a57a
ErizoAwards <prodestats@gmail.com>
2025-12-19 17:29:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+function toggleMusic() {
+    var music = document.getElementById("bgMusic");
+    var btn = document.getElementById("musicBtn");
+    
+    if (music.paused) {
+        music.play();
+        btn.innerHTML = "⏸️";
+        btn.style.boxShadow = "0 0 25px #00ff00";
+    } else {
+        music.pause();
+        btn.innerHTML = "🎵";
+        btn.style.boxShadow = "0 0 15px #bf953f";
+    }
+}
